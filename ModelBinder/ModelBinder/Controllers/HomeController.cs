a7317658537032ad829673383345d41d47986e97
Nila <nilavosoughzadeh@gmail.com>
2025-06-23 01:10:56 +0330

    ModelBinder

---
+++ ModelBinder/ModelBinder/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string name , string family , int? age)
+        {
+            if (age != null & age < 18)
+            {
+                ViewBag.Message = "سن شما قانونی نیست بچه جون";
+                return View("_Message");
+            }
+            ViewBag.name = name;
+            ViewBag.family = family;
+            ViewBag.age = age;
+            return View();
+        }
+    }
+}
