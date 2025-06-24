b3f713ada8fbae310663da357399e6e038e6583a
Nila <nilavosoughzadeh@gmail.com>
2025-06-25 01:13:14 +0330

    PartialView

---
+++ PartialView/PartialView/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestPartial()
+        {
+            ViewBag.Site = "google.com";
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult LoadPartial(int id = 0)
+        {
+            switch (id)
+            {
+                case 1:
+                    return PartialView("_PartialPage1");
+                case 2:
+                    return PartialView("_PartialPage2");
+                default:
+                    return null;
+            }
+        }
+    }
+}
