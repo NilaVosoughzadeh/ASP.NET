c24a2388da8b1633cab7344c7242fb40e6e7a5a2
Nila <nilavosoughzadeh@gmail.com>
2025-06-25 23:13:01 +0330

    Route

---
+++ Route/Route/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Route.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("ContactUs")]
+        public ActionResult ContactUs()
+        {
+        return View();
+        }
+
+        [Route("AboutUs")]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        //? for optional
+        [Route("Person/{name}/{family?}")]
+        public ActionResult ShowPerson(string name , string family)
+        {
+            ViewBag.Name = name;
+            ViewBag.Family = family;
+            return View();
+        }
+    }
+}
