45a1abcafc98f3fe0ae1fdc1c08616673d805cb2
Nila <nilavosoughzadeh@gmail.com>
2025-06-27 13:15:15 +0330

    ViewBag

---
+++ ViewBag/ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Mvc;
+
+namespace ViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            dynamic data = "Boy";
+
+            ViewBag.data = "Arash";
+
+            List<string> names = new List<string>();
+            names.Add("Nila");
+            names.Add("Vania");
+            names.Add("Arash");
+            names.Add("Rosana");
+
+            ViewBag.name = names;
+
+            return View();
+        }
+
+        public ActionResult Test(int id=0)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+    }
+}
