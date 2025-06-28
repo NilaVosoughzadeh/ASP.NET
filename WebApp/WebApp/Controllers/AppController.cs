6245b2cf3598ae84f0fec797f148d9226aaf4345
Nila <nilavosoughzadeh@gmail.com>
2025-06-29 01:59:13 +0330

    WebApp

---
+++ WebApp/WebApp/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AppController : Controller
+    {
+        public string HiMVC()
+        {
+            return "Hello World";
+        }
+        public string ShowDateTime()
+        {
+            return DateTime.Now.ToString();
+        }
+        public string Step1()
+        {
+            return "<h1>Nila</h1>";
+        }
+    }
+}
---
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
+            return View("ContactUs");
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View("About");
+        }
+    }
+}
