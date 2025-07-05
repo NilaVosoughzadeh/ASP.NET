29a95387c4174f24dfa954f86bf5bcc0cf156ece
Nila <nilavosoughzadeh@gmail.com>
2025-07-05 20:25:00 +0330

    NilaVerse

---
+++ NilaVerse/NilaVerse/Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿namespace NilaVerse.Models
+{
+    public class Cart
+    {
+        //Cart details
+        public Cart()
+        {
+            CartItems = new List<CartItem>();
+        }
+        public int OrderId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+        public void addItem(CartItem item)
+        {
+            if (CartItems.Exists(i => i.Item.Id == item.Id))
+            {
+
+            }
+            else
+            {
+                CartItems.Add(item);
+            }
+        }
+        public void removeItem(int item)
+        {
+
+        }
+    }
+}
