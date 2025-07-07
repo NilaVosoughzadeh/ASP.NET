af4160d9a8464b561a8d3bf257726061106c0a7f
Nila <nilavosoughzadeh@gmail.com>
2025-07-07 22:34:43 +0330

    NilaVerse

--- NilaVerse/NilaVerse/Models/Cart.cs
+++ NilaVerse/NilaVerse/Models/Cart.cs
@@ -13,16 +13,24 @@
         {
             if (CartItems.Exists(i => i.Item.Id == item.Id))
             {
-
+                CartItems.Find(i => i.Item.Id == item.Item.Id).Quantity += 1;
             }
             else
             {
                 CartItems.Add(item);
             }
         }
-        public void removeItem(int item)
+        public void removeItem(int itemId)
         {
-
+            var item = CartItems.SingleOrDefault(c => c.Item.Id == itemId);
+            if (item?.Quantity <= 1)
+            {
+                CartItems.Remove(item);
+            }
+            else if (item != null)
+            {
+                item.Quantity -= 1;
+            }
         }
     }
 }
