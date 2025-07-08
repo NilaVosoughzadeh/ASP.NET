2a3edf8e23da79e386b334931a4dd530091f5b81
Nila <nilavosoughzadeh@gmail.com>
2025-07-08 22:01:57 +0330

    NilaVerse

---
+++ NilaVerse/NilaVerse/Models/CartViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NilaVerse.Models
+{
+    public class CartViewModel
+    {
+        //CartViewModel details
+        public CartViewModel()
+        {
+                CartItems = new List<CartItem>();
+        }
+        public List<CartItem> CartItems { get; set; }
+        public decimal OrderTotal { get; set; }
+    }
+}
