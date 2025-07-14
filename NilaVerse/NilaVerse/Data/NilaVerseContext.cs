b00bd71e04b2f7606164a0d3e7235d09b05fd92e
Nila <nilavosoughzadeh@gmail.com>
2025-07-14 12:07:26 +0330

    NilaVerse

--- NilaVerse/NilaVerse/Data/NilaVerseContext.cs
+++ NilaVerse/NilaVerse/Data/NilaVerseContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NilaVerse.Models;
 
 namespace NilaVerse.Data
 {
     public class NilaVerseContext : DbContext
     {
+        public NilaVerseContext(DbContextOptions<NilaVerseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
     }
 }
