be3ff89785cb184b63e25e9ed26a44c460699911
Nila <nilavosoughzadeh@gmail.com>
2025-10-06 00:13:13 +0330

    ITFamily

---
+++ ITFamily/Models/Entities/Product/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITFamily.Models.Entities.Product
+{
+    public class Product
+    {
+        [Key()]
+        [Display(Name = "شناسه")]
+        public int Id { get; set; }
+
+        [Display(Name = "نام محصول")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string? ProductName { get; set; }
+
+        [Display(Name = "قیمت (تومان)")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public float? Price{ get; set; }
+
+        [Display(Name = "درصد تخفیف")]
+        public int? Off{ get; set; }
+
+        [Display(Name = "توضیحات محصول")]
+        public string? ProductDescription { get; set; }
+
+        //relation        
+        public int? CategoryRefId { get; set; }
+
+        [ForeignKey("CategoryRefId")]
+        public virtual ProductCategory? Category{ get; set; }
+    }
+}
---
+++ ITFamily/Models/Entities/Product/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITFamily.Models.Entities.Product
+{
+    public class ProductCategory
+    {
+        [Key()]
+        [Display(Name = "شناسه")]
+        public int Id { get; set; }
+
+        [Display(Name = "عنوان دسته بندی")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string? CategoryTitle { get; set; }
+
+        [Display(Name = "ترتیب نمایش")]
+        public int? Sort { get; set; }
+
+
+        [Display(Name = "تصویر")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string? CategoryPhoto { get; set; }
+
+    }
+}
