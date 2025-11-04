0e65be9cd0b38600cc5cebe653c1ffafc4fba2dd
Nila <nilavosoughzadeh@gmail.com>
2025-11-04 12:02:57 +0330

    CMS

--- CMS/CMS/Controllers/HomeController.cs
+++ CMS/CMS/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CMS.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
